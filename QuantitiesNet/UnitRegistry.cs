9f7a5c3093ffc9c070899526903fd49a3e0c5594
mspielberg <9729801+mspielberg@users.noreply.github.com>
2022-04-04 15:18:24 -0700

    Change to List to express unit preference order

--- QuantitiesNet/UnitRegistry.cs
+++ QuantitiesNet/UnitRegistry.cs
@@ -6,11 +6,11 @@ namespace QuantitiesNet
     {
         public static UnitRegistry Default { get; } = new UnitRegistry();
 
-        private readonly Dictionary<Dimension, HashSet<Unit>> units = new Dictionary<Dimension, HashSet<Unit>>();
+        private readonly Dictionary<Dimension, List<Unit>> units = new Dictionary<Dimension, List<Unit>>();
         private readonly Dictionary<Dimension, Unit> preferredUnits = new Dictionary<Dimension, Unit>();
 
-        public bool TryGetUnits(Dimension d, out HashSet<Unit> result) => units.TryGetValue(d, out result);
-        public bool TryGetUnits<D>(out HashSet<Unit> result) where D : IDimension, new() =>
+        public bool TryGetUnits(Dimension d, out List<Unit> result) => units.TryGetValue(d, out result);
+        public bool TryGetUnits<D>(out List<Unit> result) where D : IDimension, new() =>
             TryGetUnits(Dimension.ForType<D>(), out result);
 
         public bool TryGetPreferredUnit(Dimension d, out Unit result) => preferredUnits.TryGetValue(d, out result);
@@ -19,11 +19,12 @@ namespace QuantitiesNet
 
         public void SetPreferredUnit(Unit unit) => preferredUnits[unit.Dimension] = unit;
 
-        public void Add(Unit u)
+        public void Add(Unit unit)
         {
-            if (!units.TryGetValue(u.Dimension, out var set))
-                units[u.Dimension] = set = new HashSet<Unit>();
-            set.Add(u);
+            if (!units.TryGetValue(unit.Dimension, out var list))
+                units[unit.Dimension] = list = new List<Unit>();
+            if (!list.Contains(unit))
+                list.Add(unit);
         }
     }
 }
